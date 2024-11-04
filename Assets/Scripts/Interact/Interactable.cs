d814c40ad5f4a94155d46dd602b7112589ba0139
Max <maxime.harry@e-artsup.net>
2024-11-04 14:32:48 +0100

    Delete anykey from Interactable script

--- Assets/Scripts/Interact/Interactable.cs
+++ Assets/Scripts/Interact/Interactable.cs
@@ -3,17 +3,6 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     /// <summary>
-    /// Update de test sans le personnage
-    /// </summary>
-    private void Update()
-    {
-        if (Input.anyKey)
-        {
-            Interact();
-        }    
-    }
-
-    /// <summary>
     /// Ce lancera lorsque le joueur interragira avec
     /// </summary>
     public virtual void Interact()
