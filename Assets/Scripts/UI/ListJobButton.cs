3550f7d5501734d50067525c79e5cbb2bcbc64a1
Max <maxime.harry@e-artsup.net>
2024-11-13 14:27:29 +0100

    Add Job Descripiton menu

--- Assets/Scripts/UI/ListJobButton.cs
+++ Assets/Scripts/UI/ListJobButton.cs
@@ -10,15 +10,42 @@ public class ListJobButton : MonoBehaviour
     [SerializeField]
     private GameObject _allJob;
 
+    [SerializeField]
+    private GameObject _jobDescriptionTab;
+
+    [SerializeField]
+    private List<GameObject> _jobsDescription = new List<GameObject>();
+
     public void LevelJobList()
     {
         _levelJob.SetActive(true);
         _allJob.SetActive(false);
+        _jobDescriptionTab.SetActive(false);
+        foreach (var job in _jobsDescription)
+        {
+            job.SetActive(false);
+        }
     }
 
     public void AllJobList()
     {
         _levelJob.SetActive(false);
         _allJob.SetActive(true);
+        _jobDescriptionTab.SetActive(false);
+        foreach (var job in _jobsDescription)
+        {
+            job.SetActive(false);
+        }
+    }
+
+    public void CloseWindow()
+    {
+        _levelJob.SetActive(false);
+        _allJob.SetActive(false);
+        _jobDescriptionTab.SetActive(false);
+        foreach (var job in _jobsDescription)
+        {
+            job.SetActive(false);
+        }
     }
 }
