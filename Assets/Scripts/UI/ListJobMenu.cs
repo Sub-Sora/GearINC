bec4b38627082d91e90ea20c79431f9a5e4243fc
Max <maxime.harry@e-artsup.net>
2024-11-13 09:49:39 +0100

    WIP Fiche Métier

---
+++ Assets/Scripts/UI/ListJobMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListJobMenu : MonoBehaviour
+{
+    private void LevelJobList()
+    {
+
+    }
+
+    private void AllJobList()
+    {
+
+    }
+}
