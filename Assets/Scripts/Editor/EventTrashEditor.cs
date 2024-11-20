0aa0da2bd4394248df7245bb1d230b1a52b10f75
Aurelien Tavernier <aurelientav@gmail.com>
2024-11-20 16:44:12 +0100

    visual effect add to know which point name

--- Assets/Scripts/Editor/EventTrashEditor.cs
+++ Assets/Scripts/Editor/EventTrashEditor.cs
@@ -11,11 +11,19 @@ public class EventTrashEditor : Editor
         Debug.Log("gui");
         for (int i = 0; i < gizmo.points.Count; i++)
         {
-            // Utiliser Handles pour cr�er un handle de position ajustable pour chaque point
+            // Afficher le label pour chaque point
+            Handles.Label(gizmo.transform.position + gizmo.points[i], $"Trash Spawn: {i}", new GUIStyle
+            {
+                fontStyle = FontStyle.Bold,
+                normal = new GUIStyleState { textColor = Color.white }
+            });
+
+            // Manipuler la position avec PositionHandle
             Vector3 newPosition = Handles.PositionHandle(gizmo.transform.position + gizmo.points[i], Quaternion.identity);
+
+            // Vérifier si le point a changé de position
             if (gizmo.points[i] != newPosition - gizmo.transform.position)
             {
-                // Enregistrer la modification dans l'historique pour permettre les annulations
                 Undo.RecordObject(gizmo, "Move Point");
                 gizmo.points[i] = newPosition - gizmo.transform.position;
             }
