aa0d93f6a5cf749f2e9aa04376cf899653a1a55e
Aurelien Tavernier <aurelientav@gmail.com>
2024-12-10 15:50:18 +0100

    new test idle button

--- Assets/Scripts/UI/UIJobName/ChangeWordColor.cs
+++ Assets/Scripts/UI/UIJobName/ChangeWordColor.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ChangeWordColor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private TextMeshProUGUI m_TextMeshPro;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
