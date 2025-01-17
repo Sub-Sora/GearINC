92a4567210ba614d5032c2a3f73014075729c7ea
Aurelien Tavernier <aurelientav@gmail.com>
2025-01-17 15:25:54 +0100

    begin circle

---
+++ Assets/Scripts/Interact/AreaEngine/AreaEngineEffect.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class AreaEngineEffect : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _circle;
+
+    [SerializeField]
+    private Material _classicMat;
+
+    [SerializeField]
+    private Material _badMat;
+
+    [SerializeField]
+    private Material _goodMat;
+
+
+}
